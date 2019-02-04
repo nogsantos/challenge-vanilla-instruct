5b1600badf6a800921df1611b62b06d7836c83b6
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-04 21:13:19 -0200

    feat(browser): Markup

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,7 +6,7 @@ const runSequence = require('run-sequence');
 module.exports = (gulp, options, plugins) => {
 	gulp.task('watch', () => {
 		gulp.watch(['src/js/**/*.js'], () => runSequence('js:dev', 'dev'));
-		gulp.watch(['src/scss/*.scss'], () => runSequence('css', 'dev'));
+		gulp.watch(['src/scss/**/*.scss'], () => runSequence('css', 'dev'));
 		gulp.watch(['src/*.html'], () => runSequence('dev'));
 	});
 };
